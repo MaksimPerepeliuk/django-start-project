05b70ce660c8e64200737311cbf8f394e2d15659
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-07-05 17:52:05 +0300

    fixed uresolved import in vscode

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -15,10 +16,11 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
-    
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+
     class Meta:
         ordering = ('-publish',)
-        
+
     def __str__(self):
         return self.title
