e740a0e6401ea769df6f1bb2d0a7c598fa4af6f2
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-07-05 18:35:48 +0300

    make migration from model in db

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-    title = models.CharField(max_lingth=250)
-    slug = models.SlugField(max_lingth=250, unique_for_date='publish')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
